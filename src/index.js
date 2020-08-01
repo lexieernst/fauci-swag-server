6313528c205fb25cf88fc73265e79870d81016e2
Lexie Ernst <lexieernst@Lexies-MacBook-Pro-4.local>
2020-08-01 17:18:17 -0400

    send payment to stripe

--- src/index.js
+++ src/index.js
@@ -1,18 +1,17 @@
 const app = require('express')()
-// const cors = require('cors')()
+const cors = require('cors')
 const bodyParser = require('body-parser')
 const port = 4000
 const host = 'localhost'
 const createPayment = require('./stripe').createPayment
 
-// app.use(cors())
+app.use(cors())
 app.use(bodyParser.urlencoded({
 	extended: true
 }))
 
 app.get('/secret', async (req, res)=>{
 	const intent = await createPayment()
-	console.log(intent)
 	res.json({client_secret:intent.client_secret})
 })
 
